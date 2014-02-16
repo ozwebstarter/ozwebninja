7785c5ada94c3df18df5af3bcb26ed320d7cecd6
ozwebstarter <ozwebstarter@gmail.com>
2014-02-16 12:15:40 +1000

    Added post controller
    Create posts and show the new post

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Ozwebninja::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :posts
+
   # You can have the root of your site routed with "root"
-  root 'blog#index'
+  root "blog#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
